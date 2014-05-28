5fb9764a830c847c58a390e4e5c2fa7dcb374d7d
discomethod <kolkork@gmail.com>
2014-05-28 16:57:31 +0100

    Update models.py
    added character attributes

--- web/models.py
+++ web/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 
+CHARACTER_ATTRIBUTES = ("Strength","Dexterity","Intelligence","Charisma","Vitality","Wisdom",)
+
 # Create your models here.
+
+
+class Character(models.Model):
+    for attribute in CHARACTER_ATTRIBUTES:
+        character_attribute[attribute] = models.IntegerField(default=1)
+
+class ItemSlot(models.Model):
+    itemslot_name = models.CharField(max_length=64)
 class ItemPrefix(models.Model):
     itemprefix_name = models.CharField(max_length=64)
     itemprefix_flat_strength = models.IntegerField(default=0)
@@ -10,3 +20,4 @@ class Item(models.Model):
     item_level = models.IntegerField(default=0)
     item_type = models.ForeignKey('ItemType')
     item_prefixes = models.ManyToManyField(ItemPrefix)
+    item_itemslot = models.ManyToManyField(ItemSlot) # an item can occupy multiple slots
