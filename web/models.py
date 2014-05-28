80e781c2671869a59943b7c255cb2375a82244c3
discomethod <kolkork@gmail.com>
2014-05-28 16:42:20 +0100

    Update models.py
    added max_length attributes for CharFields

--- web/models.py
+++ web/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 class ItemPrefix(models.Model):
-    itemprefix_name = models.CharField()
+    itemprefix_name = models.CharField(max_length=64)
     itemprefix_flat_strength = models.IntegerField(default=0)
 class ItemType(models.Model):
-    itemtype_name = models.CharField()
+    itemtype_name = models.CharField(max_length=64)
 class Item(models.Model):
     item_level = models.IntegerField(default=0)
     item_type = models.ForeignKey('ItemType')
