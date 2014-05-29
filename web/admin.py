d97d268232e23a4de515726d01518dc9c2dc8c62
discomethod <kolkork@gmail.com>
2014-05-29 17:49:28 +0100

    added ItemSlot and ItemType to admin

--- web/admin.py
+++ web/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from web.models import Modifier, Item, ItemPrefix, ItemSuffix
+from web.models import Modifier, Item, ItemPrefix, ItemSuffix, ItemType, ItemSlot
 
 admin.site.register(Modifier)
 admin.site.register(Item)
+admin.site.register(ItemType)
+admin.site.register(ItemSlot)
 admin.site.register(ItemPrefix)
 admin.site.register(ItemSuffix)
