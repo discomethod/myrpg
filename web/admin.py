bb975f6a8ccd14caabbb4612111465fbfd74d1fd
discomethod <kolkork@gmail.com>
2014-05-29 17:47:47 +0100

    added Item, ItemPrefix, ItemSuffix to admin
    admin will now allow editing of Item, ItemPrefix, and ItemSuffix

--- web/admin.py
+++ web/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from web.models import Modifier
+from web.models import Modifier, Item, ItemPrefix, ItemSuffix
 
 admin.site.register(Modifier)
-
+admin.site.register(Item)
+admin.site.register(ItemPrefix)
+admin.site.register(ItemSuffix)
