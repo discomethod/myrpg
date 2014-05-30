429fd608b6128ca975a07d7b5d49bd9bd7b990d2
discomethod <kolkork@gmail.com>
2014-05-30 15:49:47 +0100

    itemgen view, added prefix & suffix lists context

--- web/views.py
+++ web/views.py
@@ -6,11 +6,17 @@ from web.models import Item
 
 # Create your views here.
 def index(request):
+    # not used...
     return HttpResponse("Hello, world. You're at the web index.")
 
 def itemgen(request):
     item_list = Item.objects.filter(base__isnull=True).order_by('name')
-    context = {'item_list': item_list}
+    prefix_list = ItemPrefixGroup.objects.order_by('name')
+    suffix_list = ItemSuffixGroup.objects.order_by('name')
+    context = {'item_list': item_list,
+               'prefix_list': prefix_list,
+               'suffix_list': suffix_list,
+               }
     return render(request, 'web/itemgen.html', context)
 
 def itemlist(request):
