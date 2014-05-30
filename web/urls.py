efd043ce0b64caa000e9eb7706cfd9e61975be07
disco <kolkork@gmail.com>
2014-05-30 12:02:05 +0000

    implemented templated views

---
+++ web/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import patterns, url
+
+from web import views
+
+urlpatterns = patterns('',
+    url(r'^item/(?P<item_id>\d+)/$', views.item, name='item'),
+    url(r'^item/$', views.itemlist, name='itemlist'),
+    url(r'^$', views.index, name='index')
+)
+
--- web/views.py
+++ web/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.template import RequestContext, loader
+
+from web.models import Item
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the web index.")
+
+def itemlist(request):
+    item_list = Item.objects.order_by('name')
+    template = loader.get_template('web/index.html')
+    context = RequestContext(request, {
+        'item_list': item_list,
+    })
+    return HttpResponse(template.render(context))
+
+def item(request, item_id):
+    return HttpResponse("You are looking at item %s." % item_id)
+
