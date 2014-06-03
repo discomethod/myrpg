c8ebf097b6bcd57cc5e58b03968334705436594c
disco <kolkork@gmail.com>
2014-06-03 18:36:09 +0000

    changed item list view to plural, i.e. "/items/"

--- web/urls.py
+++ web/urls.py
@@ -5,10 +5,9 @@ from web import views
 urlpatterns = patterns('',
     url(r'^item/(?P<item_id>\d+)/$', views.item, name='item'),
     url(r'^item/gen/$', views.itemgen, name='itemgen'),
-    url(r'^item/$', views.itemlist, name='itemlist'),
+    url(r'^items/$', views.itemlist, name='itemlist'),
     url(r'^affixes/g/(?P<affixgroup_id>\d+)/$', views.affixgroup, name='affixgroup'),
     url(r'^affixes/(?P<affix_id>\d+)/$', views.affix, name='affix'),
     url(r'^affixes/$', views.affixlist, name='affixlist'),
     url(r'^$', views.index, name='index')
 )
-
