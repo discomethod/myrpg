d9e79c66288da186ea5a2c9b4483c1d2d1399f73
antong liu <discomethod@qq.com>
2014-07-17 23:55:07 +0100

    implement raresuffixes
    at this time, simply displays an "A" in all fields

--- web/aviews.py
+++ web/aviews.py
@@ -6,15 +6,15 @@ from web.models import ItemType, ItemRareSuffix
 @login_required
 @permission_required('web.edit_itemraresuffix')
 def rare_suffixes(request):
-    itemTypes = ItemType.objects.order_by("name")
-    itemRareSuffixes = ItemRareSuffix.objects.order_by("name")
-    itemMatrix = [[False for x in range(len(itemRareSuffixes))] for x in range(len(itemTypes))]
-    for sindex, itemRareSuffix in enumerate(itemRareSuffixes):
-        for tindex, itemType in enumerate(itemTypes):
-            if itemRareSuffix in itemType.raresuffixes:
-                itemMatrix[sindex][tindex] = True
+    item_types = ItemType.objects.order_by("name")
+    item_rare_suffixes = ItemRareSuffix.objects.order_by("name")
+    item_matrix = [[False for x in range(len(item_types))] for x in range(len(item_rare_suffixes))]
+    for sindex, item_rare_suffix in enumerate(item_rare_suffixes):
+        for tindex, item_type in enumerate(item_types):
+            if item_type.raresuffixes.filter(pk=item_rare_suffix.pk):
+                item_matrix[sindex][tindex] = True
     context = {'header_tab': 'items',
-                'itemTypes': itemTypes,
-                'itemRaresuffixes': itemRareSuffixes,
-                'itemMatrix': itemMatrix,}
-    
+                'item_types': item_types,
+                'item_rare_suffixes': item_rare_suffixes,
+                'item_matrix': item_matrix,}
+    return render(request,'web/admin/raresuffixes.html', context)
