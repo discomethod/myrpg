a2f27b4a3bc22075d6f35b650283eed04a059f3e
antong liu <discomethod@qq.com>
2014-07-17 21:42:14 +0100

    outline rare_suffixes implementation

--- web/aviews.py
+++ web/aviews.py
@@ -1,7 +1,20 @@
 from django.contrib.auth.decorators import login_required, permission_required
 from django.shortcuts import render
 
+from web.models import ItemType, ItemRareSuffix
+
 @login_required
 @permission_required('web.edit_itemraresuffix')
 def rare_suffixes(request):
-	pass
+    itemTypes = ItemType.objects.order_by("name")
+    itemRareSuffixes = ItemRareSuffix.objects.order_by("name")
+    itemMatrix = [[False for x in range(len(itemRareSuffixes))] for x in range(len(itemTypes))]
+    for sindex, itemRareSuffix in enumerate(itemRareSuffixes):
+        for tindex, itemType in enumerate(itemTypes):
+            if itemRareSuffix in itemType.raresuffixes:
+                itemMatrix[sindex][tindex] = True
+    context = {'header_tab': 'items',
+                'itemTypes': itemTypes,
+                'itemRaresuffixes': itemRareSuffixes,
+                'itemMatrix': itemMatrix,}
+    
